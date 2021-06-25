b63b802732e1d8f4185ce2b6f18e8be39da00c47
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-06-25 17:56:37 +0200

    Added  obtain token view

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from api import views as api_views
-
+from rest_framework.authtoken import views
 urlpatterns = [
     path('customers/', api_views.CustomerView.as_view(), name="customer"),
-    path('customers/<int:pk>/', api_views.CustomerDetailView.as_view(), name="customer-detail")
+    path('customers/<int:pk>/', api_views.CustomerDetailView.as_view(), name="customer-detail"),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
